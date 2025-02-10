f1d05b1f1a1178104b65cbfd12024990992fc813
gysiang <gysiang@gmail.com>
2025-02-10 15:02:35 +0800

    load database

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:12:39 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/02/10 11:27:39 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:45:35 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,41 @@
 # define BITCOIN_EXCHANGE_HPP
 
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <sstream>
+#include <cstdlib>
 
 class Bitcoin_Exchange
 {
 	private:
+		std::map<std::string, double> _priceData;
+
+		bool isValidDate(const std::string &date) const;
+		bool isValidValue(const std::string &value) const;
+		std::string findPreviousDate(const std::string &date) const;
 
 	public:
 		Bitcoin_Exchange();
 		~Bitcoin_Exchange();
+
+		// load the database
+		void loadDatabase(const std::string &filename);
+		// process text file
+		void processInput(const std::string &filename);
+		// get the closest earlier date price
+		double getClosestPrice(const std::string &date) const;
+
+		class BitcoinException : public std::exception
+		{
+			private:
+				std::string _message;
+			public:
+				explicit BitcoinException(const std::string &msg) : _message(msg) {}
+				virtual const char* what() const throw() { return _message.c_str(); }
+		};
 
 };
 
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,8 +6,49 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:13:43 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/02/10 11:13:57 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/02/10 15:02:20 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/BitcoinExchange.hpp"
+
+
+Bitcoin_Exchange::Bitcoin_Exchange() {};
+
+Bitcoin_Exchange::~Bitcoin_Exchange() {};
+
+void Bitcoin_Exchange::loadDatabase(const std::string &filename)
+{
+	std::ifstream file(filename);
+	if (!file.is_open())
+	{
+		throw BitcoinException("Error: Could not open datafile file.");
+	}
+	std::string line;
+	while (std::getline(file, line))
+	{
+		std::stringstream ss(line);
+		std::string date, priceStr;
+
+		// read the date and price
+		if (!std::getline(ss, date, ',') || !std::getline(ss, priceStr))
+		{
+			throw BitcoinException("Error: Invalid line format in database.");
+		}
+		// check if the date is valid
+		if (!isValidDate(date))
+		{
+			throw BitcoinException("Error: Invalid date format in database: " + date);
+		}
+		// convert the type of the price to a double to save to our struct
+		char *endPtr;
+		double price;
+
+		price = std::strtod(priceStr.c_str(), &endPtr);
+		if (*endPtr != '\0' || price < 0)
+		{
+			throw BitcoinException("Error: Invalid price value in database: " + priceStr);
+		}
+		_priceData[date] = price;
+	}
+}
