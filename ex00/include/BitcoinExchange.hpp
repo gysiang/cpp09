64e9f998852b79a0462c24218cd40e6c73a6496f
gysiang <gysiang@gmail.com>
2025-02-10 16:48:23 +0800

    check date format

--- ex00/include/BitcoinExchange.hpp
+++ ex00/include/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:12:39 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/02/10 14:45:35 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/02/10 16:45:52 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ class Bitcoin_Exchange
 		std::map<std::string, double> _priceData;
 
 		bool isValidDate(const std::string &date) const;
-		bool isValidValue(const std::string &value) const;
-		std::string findPreviousDate(const std::string &date) const;
+		double isValidValue(const std::string &value) const;
+		bool isLeapYear(int year) const;
 
 	public:
 		Bitcoin_Exchange();
@@ -40,6 +40,7 @@ class Bitcoin_Exchange
 		void processInput(const std::string &filename);
 		// get the closest earlier date price
 		double getClosestPrice(const std::string &date) const;
+		std::string findPreviousDate(const std::string &date) const;
 
 		class BitcoinException : public std::exception
 		{
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:13:43 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/02/10 15:02:20 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/02/10 16:46:22 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,59 @@ void Bitcoin_Exchange::loadDatabase(const std::string &filename)
 			throw BitcoinException("Error: Invalid date format in database: " + date);
 		}
 		// convert the type of the price to a double to save to our struct
-		char *endPtr;
-		double price;
-
-		price = std::strtod(priceStr.c_str(), &endPtr);
-		if (*endPtr != '\0' || price < 0)
-		{
-			throw BitcoinException("Error: Invalid price value in database: " + priceStr);
-		}
+		double price = isValidValue(priceStr);
 		_priceData[date] = price;
 	}
+}
+
+bool Bitcoin_Exchange::isLeapYear(int year) const
+{
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+bool Bitcoin_Exchange::isValidDate(const std::string &date) const
+{
+	// split the string into yyyy, mm and dd
+	std::stringstream ss(date);
+	std::string d1, d2, d3;
+
+	if (!std::getline(ss, d1, '-') || !std::getline(ss, d2, '-') || !std::getline(ss, d3))
+		return (false);
+
+	int year = std::atoi(d1.c_str());
+	int month = std::atoi(d2.c_str());
+	int day = std::atoi(d3.c_str());
+
+	if (year < 1980 || year > 2050)
+		return (false);
+	if (month < 1 || month > 12)
+		return (false);
+	int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	if (month == 2 && Bitcoin_Exchange::isLeapYear(year))
+	{
+		daysInMonth[1] = 29;
+	}
+	if (day < 1 || day > daysInMonth[month - 1])
+		return false;
+	return (true);
+}
+
+
+double Bitcoin_Exchange::isValidValue(const std::string &value) const
+{
+	char *endPtr;
+	double p;
+
+	p = std::strtod(value.c_str(), &endPtr);
+	if (*endPtr != '\0' || p < 0)
+	{
+		throw BitcoinException("Error: Invalid price value in database: " + value);
+	}
+	return (p);
+}
+
+std::string Bitcoin_Exchange::findPreviousDate(const std::string &date) const
+{
+
 }
