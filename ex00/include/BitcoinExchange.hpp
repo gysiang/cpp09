7947caa73d01e920ecbd513964ce079f1f565787
gysiang <gysiang@gmail.com>
2025-02-10 11:28:12 +0800

    added incomplete hpp file

---
+++ ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 11:12:39 by gyong-si          #+#    #+#             */
+/*   Updated: 2025/02/10 11:27:39 by gyong-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+
+#include <iostream>
+
+class Bitcoin_Exchange
+{
+	private:
+
+	public:
+		Bitcoin_Exchange();
+		~Bitcoin_Exchange();
+
+};
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 11:13:33 by gyong-si          #+#    #+#             */
+/*   Updated: 2025/02/10 11:27:15 by gyong-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		std::cerr << "Error: could not open file.\n";
+		return (1);
+	}
+	else
+	{
+		// process the file
+	}
+	return (0);
+}
