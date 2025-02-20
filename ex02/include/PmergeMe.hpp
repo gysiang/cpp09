2c7c091a7a984a1613d6ca7f9c6ce2d55f56aef7
gysiang <gysiang@gmail.com>
2025-02-20 15:42:17 +0800

    need to check everything before entering into container

--- ex02/include/PmergeMe.hpp
+++ ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 09:20:08 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/02/17 14:23:57 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/02/20 15:38:30 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 #include <deque>
 #include <cstdlib>
 #include <algorithm>
+#include <ctime>
+#include <iomanip>
 
 template <typename Container>
 class PmergeMe
@@ -39,13 +41,20 @@ class PmergeMe
 			}
 			return *this;
 		}
+		size_t size() const
+		{
+			return v.size();
+		}
 		size_t	insertIntoContainer(int ac, char **av);
 		bool	isValidNumber(const std::string &token);
-		void	printContainer(const Container &a);
+		void	printContainer();
 		void	splitIntoSeq(Container &a, Container &b);
+		void	recursiveSort(Container &sequence);
+		void	merge(Container &sequence, Container &left, Container &right);
 		size_t	binarySeachInsertPos(Container &s, unsigned int num);
-		void 	mergeInsert(int ac, char **av);
-	};
+		void 	mergeInsert();
+		double	runMergeSort();
+};
 
 template <typename Container>
 bool PmergeMe<Container>::isValidNumber(const std::string &token)
@@ -87,13 +96,11 @@ size_t PmergeMe<Container>::insertIntoContainer(int ac, char **av)
 }
 
 template <typename Container>
-void PmergeMe<Container>::printContainer(const Container &a)
+void PmergeMe<Container>::printContainer()
 {
-	typename Container::const_iterator it = a.begin();
-	typename Container::const_iterator it_end = a.end();
+	typename Container::const_iterator it = v.begin();
+	typename Container::const_iterator it_end = v.end();
 
-	std::cout << "Printing out the contents" << std::endl;
-	std::cout << "-----------------------------------" << std::endl;
 	while (it != it_end)
 	{
 		std::cout << *it << " ";
@@ -144,27 +151,70 @@ size_t PmergeMe<Container>::binarySeachInsertPos(Container &s, unsigned int num)
 }
 
 template <typename Container>
-void PmergeMe<Container>::mergeInsert(int ac, char **av)
+void PmergeMe<Container>::recursiveSort(Container &sequence)
+{
+	if (sequence.size() <= 1) return;
+
+	size_t mid = sequence.size() / 2;
+	Container left(sequence.begin(), sequence.begin() + mid);
+	Container right(sequence.begin() + mid, sequence.end());
+
+	recursiveSort(left);
+	recursiveSort(right);
+	// Merge sorted halves back
+	merge(sequence, left, right);
+}
+
+template <typename Container>
+void PmergeMe<Container>::merge(Container &sequence, Container &left, Container &right)
+{
+	sequence.clear();
+	size_t i = 0;
+	size_t j = 0;
+
+	while (i < left.size() && j < right.size())
+	{
+		if (left[i] < right[j])
+			sequence.push_back(left[i++]);
+		else
+			sequence.push_back(right[j++]);
+	}
+
+	while (i < left.size()) sequence.push_back(left[i++]);
+	while (j < right.size()) sequence.push_back(right[j++]);
+}
+
+template <typename Container>
+void PmergeMe<Container>::mergeInsert()
 {
 	Container pendingSequence;
 	Container mainSequence;
 
-	insertIntoContainer(ac, av);
 	splitIntoSeq(mainSequence, pendingSequence);
-
 	std::sort(mainSequence.begin(), mainSequence.end());
-
 	for (size_t i = 0; i < pendingSequence.size(); i++)
 	{
 		unsigned int num = pendingSequence[i];
-		// find the pos of the num in the main sequence
 		size_t pos = binarySeachInsertPos(mainSequence, num);
-		// insert the number into the main sequence
 		mainSequence.insert(mainSequence.begin() + pos, num);
 	}
-	// print result
-	printContainer(mainSequence);
+	v = mainSequence;
 }
+
+template <typename Container>
+double PmergeMe<Container>::runMergeSort()
+{
+	//if (insertIntoContainer(ac, av) == 1)
+	//	return -1
+	clock_t start = clock();
+	mergeInsert();
+	clock_t end = clock();
+	double elapsed = double(end - start) / CLOCKS_PER_SEC;
+	return (elapsed);
+}
+
+
+
 
 
 #endif
